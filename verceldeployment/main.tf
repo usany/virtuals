d36dc89d6a66cbe4d1203e8ce755b42633faf7fa
charlie <ckd_qja@naver.com>
2026-09-06 17:50:36 +0900

    cern

--- verceldeployment/main.tf
+++ verceldeployment/main.tf
@@ -12,7 +12,7 @@ resource "vercel_project" "example" {
   framework = "nextjs"
   git_repository = {
     type = "github"
-    repo = "<username>/nextjs-terraform-demo"
+    repo = "usany/nextjs-terraform-demo"
   }
 }
 
