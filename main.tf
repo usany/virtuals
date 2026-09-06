f19fe4241ee42d7c36b10042a3bf1860305a2a28
charlie <ckd_qja@naver.com>
2026-09-06 17:27:46 +0900

    cern

--- main.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_core_vcn" "internal" {
-  dns_label      = "internal"
-  cidr_block     = "172.16.0.0/20"
-  compartment_id = "<your_compartment_OCID_here>"
-  display_name   = "My internal VCN"
-}
