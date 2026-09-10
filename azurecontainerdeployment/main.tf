f2b0b8290176dc89cfb430df4b43a4b82cc31570
charlie <ckd_qja@naver.com>
2026-09-10 20:11:33 +0900

    cern

--- azurecontainerdeployment/main.tf
+++ azurecontainerdeployment/main.tf
@@ -2,14 +2,11 @@ data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
-<<<<<<< HEAD
-=======
 data "azurerm_container_registry" "remake" {
   name                = "remake"
   resource_group_name = "DefaultResourceGroup-SE"
 }
 
->>>>>>> main
 resource "random_string" "dns_suffix" {
   length  = 5
   special = false
@@ -41,15 +38,12 @@ resource "azurerm_container_group" "containers" {
   os_type             = "Linux"
   ip_address_type     = var.enable_public_ip ? "Public" : "Private"
   dns_name_label      = var.enable_public_ip ? "${each.key}-${var.environment}-${random_string.dns_suffix.result}" : null
-<<<<<<< HEAD
-=======
 
   image_registry_credential {
     server   = data.azurerm_container_registry.remake.login_server
     username = data.azurerm_container_registry.remake.admin_username
     password = data.azurerm_container_registry.remake.admin_password
   }
->>>>>>> main
 
   container {
     name   = each.key
